637655700a7bcb4c3697848fb6f0d71fd76a85e6
Benjamin Zaslavsky <benjamin.zaslavsky@manomano.com>
2018-08-27 17:58:07 +0200

    Fin p1

--- Cours_Construisez_une_interface_utilisateur_flexible_et_adaptative/Partie 1/ToolMyBar/app/src/main/java/com/openclassrooms/toolmybar/MainActivity.java
+++ Cours_Construisez_une_interface_utilisateur_flexible_et_adaptative/Partie 1/ToolMyBar/app/src/main/java/com/openclassrooms/toolmybar/MainActivity.java
@@ -1,13 +1,64 @@
 package com.openclassrooms.toolmybar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageViewOC;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.configureToolbar();
+        this.configureImageView();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_activity_main_more:
+                Toast.makeText(this, "Il n'y a rien à paramétrer ici...", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_activity_main_search:
+                Toast.makeText(this, "Il n'y a rien à chercher ici non-plus...", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void configureImageView() {
+        this.imageViewOC = (ImageView) findViewById(R.id.imageView);
+        imageViewOC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchDetailActivity();
+            }
+        });
+    }
+
+    private void launchDetailActivity() {
+        Intent myIntent = new Intent(MainActivity.this, DetailActivity.class);
+        this.startActivity(myIntent);
     }
 }
